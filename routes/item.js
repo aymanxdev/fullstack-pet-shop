f2387fc58ba6d3af9bee4ca727cd9f48bdcd482d
aymanxdev <ayman.bacc@gmail.com>
2021-07-23 14:46:43 +0100

    add auth and item routes

---
+++ routes/item.js
@@ -0,0 +1,11 @@
+const { Router } = require("express");
+const itemConroller = require('../controllers/itemControllers')
+const router = Router()
+
+router.get('item', itemConroller.get_items)
+router.post('items', itemConroller.post_item)
+router.putt('items/:id', itemConroller.update_item)
+router.delete('items/:id', itemConroller.delete_item)
+
+
+module.exports = router
