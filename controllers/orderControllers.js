3fd368aaf0f5d0909a4b4ed504d95aeee01b23aa
aymanxdev <ayman.bacc@gmail.com>
2021-07-24 22:39:01 +0100

    add stripe checkout function in Order controller

--- controllers/orderControllers.js
+++ controllers/orderControllers.js
@@ -13,11 +13,37 @@ module.exports.get_orders = async (req, res) => {
 module.exports.checkout = async (req, res) => {
 
     try {
-        
+        const userId = req.params.id
+        const {source} = req.body 
+        let cart = await Cart.findOne({userId})
+        let user = await User.findOne({_id: userId})
+        const email = user.email
+
+        if(cart){
+            const charge = await stripe.charges.create({
+                amount: cart.bill,
+                currency: "usd",
+                source: source,
+                receipt_email: email
+            })
+            if (!charge) throw Error("Payment Failed")
+            if(charge) {
+                const order = await Order.create({
+                    userId,
+                    items: cart.items,
+                    bill: cart.bill,
+                });
+                const data = await Cart.findByIdAndDelete({_id:cart.id})
+                return res.status(201).send(order)
+            }
+        }else {
+            res.status(500).send("Your cart is empty. Please add items")
+        }
 
     }
     catch (error) {
-
+        console.log(error);
+        res.status(500).send("something went wrong")
     }
 
 }
