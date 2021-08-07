14650d6b038b248ac397a6e39878d6be6349a096
aymanxdev <ayman.bacc@gmail.com>
2021-08-07 22:57:43 +0100

    add items or products reducers

---
+++ client/src/reducers/itemReducer.js
@@ -0,0 +1,52 @@
+import {
+  GET_ITEMS,
+  ADD_ITEM,
+  UPDATE_ITEM,
+  DELETE_ITEM,
+  ITEMS_LOADING,
+} from "../actions/type";
+
+const initialState = {
+  items: [],
+  loading: false,
+};
+
+export default function (state = initialState, action) {
+  switch (action.type) {
+    case ITEMS_LOADING:
+      return {
+        ...state,
+        loading: true,
+      };
+    case GET_ITEMS:
+      return {
+        ...state,
+        items: action.payload,
+        loading: false,
+      };
+    case ADD_ITEM:
+      return {
+        ...state,
+        items: [action.payload, ...state.items],
+      };
+    case UPDATE_ITEM:
+      const { id, data } = action.payload;
+      return {
+        ...state,
+        items: state.items.map((item) => {
+          if (item._id == id) {
+            item = data;
+          }
+        }),
+      };
+
+    case DELETE_ITEM:
+      return {
+        ...state,
+        items: state.items.filter((item) => item._id !== action.payload),
+      };
+
+    default:
+      return state;
+  }
+}
