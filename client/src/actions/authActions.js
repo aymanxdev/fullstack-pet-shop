d6e01f046680bf6011e48137cab3faac4e0b9730
aymanxdev <ayman.bacc@gmail.com>
2021-08-07 20:22:10 +0100

    add register function

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -27,3 +27,32 @@ export const loadUser = () => (dispatch, getState) => {
       dispatch({ type: AUTH_ERROR });
     });
 };
+
+export const register =
+  ({ name, email, password }) =>
+  (dispatch) => {
+    //headers
+    const config = {
+      headers: { "Content-Type": "application/json" },
+    };
+
+    //request body
+
+    const body = JSON.stringify({ name, email, password });
+    axios
+      .post("api/register", body, config)
+      .then((res) =>
+        dispatch({
+          type: REGISTER_SUCCESS,
+          payload: res.data,
+        })
+      )
+      .catch((err) => {
+        dispatch(
+          returnErrors(err.response.data, err.response.status, REGISTER_FAIL)
+        );
+        dispatch({
+          type: REGISTER_FAIL,
+        });
+      });
+  };
