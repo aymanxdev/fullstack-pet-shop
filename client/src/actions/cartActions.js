3a5338e0fb049589e52e20cafe15a8ce91897059
aymanxdev <ayman.bacc@gmail.com>
2021-08-07 21:15:18 +0100

    get cart items and to cart

---
+++ client/src/actions/cartActions.js
@@ -0,0 +1,38 @@
+import axios from "axios";
+import { returnErrors } from "./errorActions";
+import { CART_LOADING, GET_CART, ADD_TO_CART, DELTE_FROM_CART } from "./type";
+
+export const getCart = (id) => (dispatch) => {
+  dispatch(setCartLoading());
+  axios
+    .get(`/api/cart/${id}`)
+    .then((res) =>
+      dispatch({
+        type: GET_CART,
+        payload: res.data,
+      })
+    )
+    .catch((err) =>
+      dispatch(returnErrors(err.response.data, err.response.status))
+    );
+};
+
+export const addToCart = (id, productId, quantity) => (dispatch) => {
+  axios
+    .post(`/api/cart/${id}`, { productId, quantity })
+    .then((res) =>
+      dispatch({
+        type: ADD_TO_CART,
+        payload: res.data,
+      })
+    )
+    .catch((err) =>
+      dispatch(returnErrors(err.response.data, err.response.status))
+    );
+};
+
+export const setCartLoading = () => {
+  return {
+    type: CART_LOADING,
+  };
+};
