f1c635e214ff2b2ce2bd25621a3aba767f5aa2a3
aymanxdev <ayman.bacc@gmail.com>
2021-08-08 17:06:15 +0100

    add Home component

---
+++ client/src/components/Home.js
@@ -0,0 +1,80 @@
+import { Component } from "react";
+import Navbar from "./Navbar";
+import {
+  Card,
+  CardText,
+  CardBody,
+  CardTitle,
+  CardSubtitle,
+  Button,
+  Container,
+} from "reactstrap";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { getItems } from "../actions/itemActions";
+import { addToCart } from "../actions/cartActions";
+
+class Home extends Component {
+  componentDidMount() {
+    this.props.getItems();
+  }
+
+  static propTypes = {
+    getItems: PropTypes.func.isRequired,
+    item: PropTypes.object.isRequired,
+    isAuthenticated: PropTypes.bool,
+    addToCart: PropTypes.func.isRequired,
+    user: PropTypes.object.isRequired,
+  };
+
+  onAddToCart = async (id, productId) => {
+    await this.props.addToCart(id, productId, 1);
+    alert("Item added to Cart");
+  };
+
+  render() {
+    const { items } = this.props.item;
+    const user = this.props.user;
+    return (
+      <div>
+        <Navbar />
+        <Container>
+          <div className="row">
+            {items.map((item) => (
+              <div className="col-md-4">
+                <Card className="mb-4">
+                  <CardBody>
+                    <CardTitle tag="h5">{item.title}</CardTitle>
+                    <CardSubtitle tag="h6">Rs. {item.price}</CardSubtitle>
+                    <CardText>{item.category}</CardText>
+                    {this.props.isAuthenticated ? (
+                      <Button
+                        color="success"
+                        size="sm"
+                        onClick={this.onAddToCart.bind(
+                          this,
+                          user._id,
+                          item._id
+                        )}
+                      >
+                        Add To Cart
+                      </Button>
+                    ) : null}
+                  </CardBody>
+                </Card>
+              </div>
+            ))}
+          </div>
+        </Container>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state) => ({
+  item: state.item,
+  isAuthenticated: state.auth.isAuthenticated,
+  user: state.auth.user,
+});
+
+export default connect(mapStateToProps, { getItems, addToCart })(Home);
